f3c025550fee17b568fc4b561e1ccecee02e2ad4
Alvin <alvinwong220@gmail.com>
2016-02-28 04:43:47 -0500

    Fix controller

---
+++ assets/Scripts/PalmUp.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class PalmUp : MonoBehaviour {
+    Controller controller;
+    Frame frame;
+    HandList hands;
+    Hand hand;
+	// Use this for initialization
+	void Start () {
+        controller = new Controller();
+        
+	}
+
+    // Update is called once per frame
+    void Update() {
+        frame = controller.Frame();
+        hands = frame.Hands;
+        if (hands.IsEmpty)
+            return;
+        hand = hands[0];
+        if (hand.IsValid) { 
+        Debug.Log(hand.PalmNormal);
+    }
+	}
+}
